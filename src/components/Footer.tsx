f1beedd1ed2ef19d23d672196446546c3c096b92
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-05-14 10:49:23 -0400

    footer styling complete and added link to my github page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,16 @@
 const Footer = () => {
-  return <footer>Footer</footer>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className='mt-auto font-kgcs text-white'>
+      <section className='mx-auto flex max-w-6xl justify-center px-4 pb-6 pt-16'>
+        <p>
+          &copy; {currentYear}{' '}
+          <a href='https://github.com/vzMars'>Marcos Gonzalez</a>
+        </p>
+      </section>
+    </footer>
+  );
 };
 
 export default Footer;
