eff562f922bf9aeaa018f207230e7cc1a4522fc4
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-05-14 11:49:26 -0400

    AuthContext added to the app

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,80 @@
+import { createContext, useReducer, useEffect, ReactElement } from 'react';
+import Loader from '../components/Loader';
+import {
+  AuthStateType,
+  AuthContextType,
+  AuthReducerAction,
+  ChildrenType,
+} from '../types';
+
+const initialState: AuthStateType = {
+  user: null,
+  isLoading: true,
+};
+
+const initAuthContextState: AuthContextType = {
+  user: initialState.user,
+  isLoading: initialState.isLoading,
+  dispatch: () => undefined,
+};
+
+export const AuthContext = createContext<AuthContextType>(initAuthContextState);
+
+export const authReducer = (
+  state: AuthStateType,
+  action: AuthReducerAction
+): AuthStateType => {
+  switch (action.type) {
+    case 'LOGIN':
+      if (!action.payload) {
+        throw new Error('action.payload missing in LOGIN action');
+      }
+
+      return {
+        user: action.payload,
+        isLoading: false,
+      };
+    case 'LOGOUT':
+      return {
+        user: null,
+        isLoading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export const AuthProvider = ({ children }: ChildrenType): ReactElement => {
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  useEffect(() => {
+    const getAuthStatus = async () => {
+      const response = await fetch('http://localhost:8080/api/auth', {
+        method: 'GET',
+        credentials: 'include',
+      });
+
+      const json = await response.json();
+
+      if (json.user === null) {
+        dispatch({ type: 'LOGOUT' });
+      } else {
+        dispatch({ type: 'LOGIN', payload: json.user });
+      }
+    };
+
+    getAuthStatus();
+  }, []);
+
+  return (
+    <>
+      {state.isLoading ? (
+        <Loader />
+      ) : (
+        <AuthContext.Provider value={{ ...state, dispatch }}>
+          {children}
+        </AuthContext.Provider>
+      )}
+    </>
+  );
+};
