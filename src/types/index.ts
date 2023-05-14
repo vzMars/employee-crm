e5703334776f0fabe7eadb6f7ab3831e618a7d23
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-05-14 11:19:55 -0400

    types that will be used in the AuthContext and EmployeeContext

---
+++ src/types/index.ts
@@ -0,0 +1,55 @@
+import { ReactElement } from 'react';
+
+export type ChildrenType = { children?: ReactElement | ReactElement[] };
+
+// AuthContext
+export type UserType = {
+  id: string;
+  email: string;
+  username: string;
+};
+
+export type AuthStateType = {
+  user: UserType | null;
+  isLoading: boolean;
+};
+
+export type AuthContextType = {
+  user: UserType | null;
+  isLoading: boolean;
+  dispatch: React.Dispatch<AuthReducerAction>;
+};
+
+export type AuthReducerAction =
+  | { type: 'LOGIN'; payload: UserType }
+  | { type: 'LOGOUT' };
+
+// EmployeeContext
+export type EmployeeType = {
+  id: string;
+  name: string;
+  address: string;
+  city: string;
+  state: string;
+  zipcode: string;
+  jobTitle: string;
+  status: string;
+  userId: string;
+};
+
+export type EmployeeStateType = {
+  employees: EmployeeType[];
+  isLoading: boolean;
+};
+
+export type PostContextType = {
+  employees: EmployeeType[];
+  isLoading: boolean;
+  dispatch: React.Dispatch<EmployeeReducerAction>;
+};
+
+export type EmployeeReducerAction =
+  | { type: 'SET'; payload: EmployeeType[] }
+  | { type: 'CREATE'; payload: EmployeeType }
+  | { type: 'UPDATE'; payload: EmployeeType }
+  | { type: 'DELETE'; payload: string };
