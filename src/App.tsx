9145aaeabacb4fb4d015bee28058cde3d0b509c4
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-05-14 09:23:25 -0400

    using browser router to contain all the routes of this app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// Pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import CreateEmployee from './pages/CreateEmployee';
+import EmployeeDetails from './pages/EmployeeDetails';
+import UpdateEmployee from './pages/UpdateEmployee';
+import NotFound from './pages/NotFound';
+
+// Layout
+import Layout from './layouts/Layout';
+
 const App = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+
+          <Route path='login' element={<Login />} />
+          <Route path='signup' element={<Signup />} />
+
+          <Route path='create' element={<CreateEmployee />} />
+          <Route path='employee/:id' element={<EmployeeDetails />} />
+          <Route path='update/:id' element={<UpdateEmployee />} />
+
+          <Route path='404' element={<NotFound />} />
+          <Route path='*' element={<Navigate to='404' replace />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
