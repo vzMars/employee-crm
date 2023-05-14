3b37230b9695da136cc179fd081f9464001a18c4
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-05-14 16:04:36 -0400

    when user logs in/signs up retrieve all their employees

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import { useAuthContext } from './useAuthContext';
+import { useEmployeeContext } from './useEmployeeContext';
+import { getEmployees } from '../services/getEmployees';
 
 export const useLogin = () => {
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const { dispatch } = useAuthContext();
+  const { dispatch: employeeDispatch } = useEmployeeContext();
 
   const login = async (username: string, password: string) => {
     setIsLoading(true);
@@ -28,6 +31,7 @@ export const useLogin = () => {
 
     if (response.ok) {
       dispatch({ type: 'LOGIN', payload: json.user });
+      getEmployees(employeeDispatch);
       setIsLoading(false);
     }
   };
--- src/hooks/useSignup.tsx
+++ src/hooks/useSignup.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import { useAuthContext } from './useAuthContext';
+import { useEmployeeContext } from './useEmployeeContext';
+import { getEmployees } from '../services/getEmployees';
 
 export const useSignup = () => {
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const { dispatch } = useAuthContext();
+  const { dispatch: employeeDispatch } = useEmployeeContext();
 
   const signup = async (email: string, username: string, password: string) => {
     setIsLoading(true);
@@ -28,6 +31,7 @@ export const useSignup = () => {
 
     if (response.ok) {
       dispatch({ type: 'LOGIN', payload: json.user });
+      getEmployees(employeeDispatch);
       setIsLoading(false);
     }
   };
