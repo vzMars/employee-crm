0f35b27f1ac7d62159322720840e410b6cd1c9ad
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-05-14 20:41:59 -0400

    Not found page styling

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
   return (
-    <main>
-      <h1>NotFound</h1>
+    <main className='m-auto font-lexbold text-gray-50'>
+      <section className='mx-auto flex max-w-6xl flex-col items-center justify-center space-y-8 px-4 pt-32 md:pt-40'>
+        <h1 className='text-9xl font-bold text-gray-50'>404</h1>
+        <p className='lead fw-normal text-center font-medium'>
+          Oops! The page you are looking for does not exist.
+        </p>
+        <Link
+          to='/'
+          className='rounded-md bg-table-100 px-4 py-3 text-xl font-bold'
+        >
+          Back To Home
+        </Link>
+      </section>
     </main>
   );
 };
